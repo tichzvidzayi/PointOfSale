2cbf8fb93872ab512c1801567b55e1edae42ad91
Tich Zvidzayi|Analyst Developer <tzvidzie@gmail.com>
2025-08-06 08:57:46 +0200

    update

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import { signUp, logIn } from '../controllers/authController';
 
 const router = express.Router();
-
 router.post('/signup', signUp);
 router.post('/login', logIn);
 
